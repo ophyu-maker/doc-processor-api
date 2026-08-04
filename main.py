fdc0848863ca3a2ca64398ed3a31ab2fa74813fc
ophyu-maker <ppou.16@outlook.com>
2026-08-04 10:10:00 -0400

    Add text cleaner

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from src.document_processor.cleaner import TextCleaner
+from src.document_processor.models import Document
+
+
 def main():
-    print("Hello from doc-processor-api!")
+    doc = Document.from_file(doc_id="1", file_path="data/input/sample.txt")
+    cleaner = TextCleaner()
+    cleaned_text = cleaner.clean_text(doc.text)
+
+    print(f"Original Text: {doc.text}")
+    print(f"Cleaned Text: {cleaned_text}")
 
 
 if __name__ == "__main__":
---
+++ src/document_processor/cleaner.py
@@ -0,0 +1,19 @@
+from src.document_processor.exceptions import EmptyDocumentError
+
+
+class TextCleaner:
+    def __init__(self):
+        pass
+
+    def clean_text(self, text: str) -> str:
+        """
+        Cleans the input text by removing unwanted characters and formatting.
+
+        Args:
+            text (str): The input text to be cleaned."""
+        cleaned_text = " ".join(text.split())  # remove spaces.
+
+        if not cleaned_text:
+            raise EmptyDocumentError("The document contains no usable text.")
+
+        return cleaned_text
